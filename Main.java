29b5bad97a1119ddcf1714b878c67a9015d7aee2
EgorE123 <dyukin.1992@bk.ru>
2025-05-24 14:25:33 +0500

    Update Main.java
    delete

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("локалка");
+        System.out.println("лefwefwefewfewf");
         System.out.println("24.05.2025");
     }
 }
